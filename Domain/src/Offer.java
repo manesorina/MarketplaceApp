c5f293e53fcb217a1af84891cc4bc63389672c52
ana kogalniceanu <anastef10@yahoo.com>
2024-10-30 13:45:49 +0200

    Added Offer and Order classes

---
+++ Domain/src/Offer.java
@@ -0,0 +1,12 @@
+public class Offer {
+    String message;
+    double offeredPrice;
+    Object targetedObject;
+
+    public Offer(String message, double offeredPrice, Object targetedObject) {
+        this.message = message;
+        this.offeredPrice = offeredPrice;
+        this.targetedObject = targetedObject;
+    }
+
+}
