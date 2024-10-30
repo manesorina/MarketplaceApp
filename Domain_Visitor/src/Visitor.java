e4aeb9fee96a9005d46eaa2eace3e819661bf00e
so17 <manesorina@gmail.com>
2024-10-30 11:43:44 +0200

    Created Visitor class

--- Domain_Visitor/src/Visitor.java
+++ Domain_Visitor/src/Visitor.java
@@ -1,3 +1,13 @@
 public class Visitor {
+    public int Id;
+
+    public Visitor(int VId) {
+        this.Id = Id;
+    }
+
+    public String[] serchItems(){};
+
+    public String[] searchUsers(){};
+
 
 }
